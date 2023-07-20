87871dd3db29d1e0b0fc3f7e61d125627f5f1516
AnjaliCherukuri <mohananjalicherukuri@gmail.com>
2023-07-20 17:52:47 +0530

    My_Initial_Commit

---
+++ Anjali/lib/src/Condition_if_Demo.java
@@ -0,0 +1,58 @@
+public class Condition_if_Demo {
+    public static void main(String[] args) {
+        int sum=30;
+        if(sum==30)
+        {
+            System.out.println("the sum is 30 :");
+        } 
+
+        
+        //float
+        float f=32.8f;
+        if(f>=30.4)
+        {
+            System.out.println("it is a float value");//it will not take other datatypes other than float
+        }
+
+        /*char c='C';
+        if(c>=C)
+        {}*/
+         
+        //String
+        String greeting="How are you?";
+        if(greeting=="How are you?")
+        {
+            System.out.println("enquirng");
+        }
+        
+        //int
+        int num=5;
+        if(num>=0)
+        {
+            System.out.println("Number is positive");
+        }
+        
+        //boolean
+        boolean b1=true;
+        if(b1==true)
+        {
+            System.out.println("it is true");
+        }
+
+        if(b1!=true)
+        {
+            System.out.println("false");
+        }
+
+
+       /*  if(b1>=true)
+        {
+            System.out.println("false");
+        }*/
+
+         
+
+    
+    }
+  
+}
