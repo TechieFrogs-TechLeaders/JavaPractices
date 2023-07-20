a7f30d834350e33454d5e2a50e34b9e70128db62
GayathriMummadi <gayathrimummadi8@gmail.com>
2023-07-20 17:52:17 +0530

    Myinitialcommit

---
+++ Gayathri/src/Float.java
@@ -0,0 +1,6 @@
+public class Float {
+     static float b1 =68780.57f;
+     public static void main(String[]args)  {
+        System.out.println(b1);
+     }
+}
---
+++ Gayathri/src/Gayathri.java
@@ -0,0 +1,6 @@
+public class Gayathri {
+      public static void main(String[] args) {
+            System.out.println("Hello World");
+
+  }
+}
