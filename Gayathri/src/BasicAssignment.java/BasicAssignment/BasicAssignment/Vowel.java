2b5fc06956565af14a89702d66d3b25eed35746c
GayathriMummadi <gayathrimummadi8@gmail.com>
2023-07-21 17:43:43 +0530

    Myinitialcommit

---
+++ Gayathri/src/BasicAssignment.java/BasicAssignment/BasicAssignment/Vowel.java
@@ -0,0 +1,28 @@
+package BasicAssignment;
+
+public class Vowel {
+    public static void main(String[] args) {
+        char ch = 'e';
+        switch(ch){
+            case 'a':
+            case  'e':
+            case  'i':
+            case  'o':
+            case  'u' :  
+            System.out.println("is vowel");
+
+
+        
+
+    }
+
+
+
+
+
+
+
+    
+
+}
+}
