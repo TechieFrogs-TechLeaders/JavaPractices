cccd74d1c93434f9ed80b58da7031fd7947bd6cb
vallurivijaya <chinnuvijju9640@gmail.com>
2023-07-22 02:08:10 -0800

    Pyramid

--- Vijaya/src/BasicAssignment/InvertedNumbers.java
+++ Vijaya/src/BasicAssignment/InvertedNumbers.java
@@ -2,11 +2,16 @@ package BasicAssignment;
 
 public class InvertedNumbers {
    public static void main(String[] args) {
-    for(int i = 1; i <=5; i++){
-        for(int j = 1; j <=i; j++){
-            System.out.print(j);
+    int rows = 5,k = 0;
+    for(int i = 1; i <=rows;++i,k = 0){
+        for(int space = 1; space<=rows-i;++space){
+            System.out.print(" ");
    }
-     System.out.println();
+     while (k!=2*i-1){ 
+        System.out.print("*");
+        ++k;
+    }
+      System.out.println();
     }
 }
 }
