09fb5747b1a7baab637efe859d2415cfa53b8a24
vallurivijaya <chinnuvijju9640@gmail.com>
2023-07-21 21:51:22 -0800

    new change

---
+++ Vijaya/src/BasicAssignment/InvertedpyramidFull.java
@@ -0,0 +1,14 @@
+package BasicAssignment;
+
+public class InvertedpyramidFull {
+    public static void main(String[] args) {
+        int n = 8;
+        for (int i = 1 ;i <=8;i++) {
+            for (int j = 0;j<=i;j++) {
+                System.out.print(      "*");
+            }
+        }
+    }
+}
+
+
