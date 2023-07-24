b4e688076513e32eb581ad9b7feb47cd9c600a85
Likhitha <Likhithakakarla52@gmail.com>
2023-07-24 11:08:58 +0530

    Today commit

--- Likhitha/src/BasicAssignment/Lcm.java
+++ Likhitha/src/BasicAssignment/Lcm.java
@@ -2,7 +2,22 @@ package BasicAssignment;
 
 public class Lcm {
     public static void main(String[] args) {
-        
-
+        int n1=18;
+        int n2=15;
+        int temp1 = n1;
+        int temp2 = n2;
+    while(n1!=n2) {
+        if (n1>n2) {
+            n1=n1-n2;
+        }
+        else {
+            n2=n2-n1;
+        }
     }
-}
+    int lcm=(temp1*temp2 )/n1;
+          System.out.println("Lcm of" + " " + temp1 +  " " +"&&"+ " " +temp2 + " " + "Is" + " " + lcm);
+        }
+      }
+    
+
+
