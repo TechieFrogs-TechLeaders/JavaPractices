1c80716069fa37dc112173890f5ef2ae427ddecc
vallurivijaya <chinnuvijju9640@gmail.com>
2023-08-04 03:38:53 -0800

    Html changes

---
+++ Vijaya/src/Opertors.java
@@ -0,0 +1,14 @@
+public class Opertors { 
+    public static void main(String[] args) {
+        int a = 10, b = 20,c =30;
+        //unary opertor
+       // Increement
+       /*  System.out.println(++a);
+        System.out.println(b--);
+        System.out.println(c++);*/
+    
+        int number1 =12,number
+    }
+
+    }
+
