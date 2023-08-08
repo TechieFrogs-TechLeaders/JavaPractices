1fdfd6925a241cd0f46ddb057eda093581e3e757
AnjaliCherukuri <mohananjalicherukuri@gmail.com>
2023-08-08 18:14:46 +0530

    new changes

---
+++ Anjali/Practices/Form_Js.js
@@ -0,0 +1,6 @@
+function validation(){
+    document.getElementById("myj").display="fill the field";
+    document.getElementById("myj").style.color="red";
+    document.getElementById("error").innerHTML="Wrong words";
+    document.getElementById("error").hidden=false;
+}
