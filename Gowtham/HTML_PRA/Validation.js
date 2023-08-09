1c6256ea4361452d455b4403d8408c83de7f17ae
gowthamcherukuri69 <gowtham.chg@gmail.com>
2023-08-09 11:34:11 +0530

    new changes

--- Gowtham/HTML_PRA/Validation.js
+++ Gowtham/HTML_PRA/Validation.js
@@ -13,10 +13,44 @@ function validation() {
     document.getElementById("rr").style.color = "red";
     document.getElementById("tr").hidden = false;
     document.getElementById("tr").style.color = "red";
-    document.getElementById("rrr").hidden = false;
-    document.getElementById("rrr").style.color = "red";
-    document.getElementById("lr").hidden = false;
-    document.getElementById("lr").style.color = "red";
     document.getElementById("cr").hidden = false;
     document.getElementById("cr").style.color = "red";  
+
+ let x=document.getElementById("city").value;   
+if (x==""){
+    document.getElementById("x").innerHTML="city must be filled";
+    document.getElementById("x").style.color="red"
 }
+let y=document.getElementById("PinCode").value;
+if(y==""){
+    document.getElementById("lr").innerHTML="pincode must be filled";
+    document.getElementById("lr").style.color="red"
+    document.getElementById("lr").hidden=false;
+}
+var val=false;
+    if(document.getElementById("highschool").checked){
+        val=true;
+    }
+    else if(document.getElementById("inter").checked){
+    val=true;
+
+    }
+    else if(document.getElementById("college").checked){
+    val=true;
+    
+    }
+    else if(document.getElementById("university").checked){
+        val=true;
+        
+    }
+     else if(document.getElementById("phd").checked){
+    val=true;
+
+    }
+    else{
+    document.getElementById("checkbox").innerHTML="select a qualification";
+    console.log("please enter qualification")
+    document.getElementById("checkbox").style.color="red"
+    }
+        
+}                   
