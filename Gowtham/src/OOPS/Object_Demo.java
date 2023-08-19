8210678235532d5b31cea0f1601522da8e05be76
gowthamcherukuri69 <gowtham.chg@gmail.com>
2023-08-19 11:17:32 +0530

    new updates

---
+++ Gowtham/src/OOPS/Object_Demo.java
@@ -0,0 +1,11 @@
+package OOPS;
+
+public class Object_Demo {
+    int id;
+     String name; 
+     public static void main(String args[]){ 
+        Object_Demo s1=new Object_Demo();
+        System.out.println(s1.id);  
+        System.out.println(s1.name);  
+     }
+}
