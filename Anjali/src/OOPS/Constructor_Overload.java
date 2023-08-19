ce80187b8245fdbdca81ed6eec3600292cf9954c
AnjaliCherukuri <mohananjalicherukuri@gmail.com>
2023-08-19 15:45:40 +0530

    new changes

--- Anjali/src/OOPS/Constructor_Overload.java
+++ Anjali/src/OOPS/Constructor_Overload.java
@@ -10,20 +10,25 @@ public class Constructor_Overload {
         i = 34;
         j = 54;
         s = "Hello";
-
     }
-    Constructor_Overload(int a){ //a,b,s are the local variables so by using
+    Constructor_Overload(int a){ //a,b,s are the local variables so by using instance/global variables we are assigning values
         this(3,"hello",67);
          System.out.println("int");
          //this();  constructor calling should be first line in con 
          this.i = a;
     }
     Constructor_Overload(int a,int b){
-        //this(4); will be a loop error/recursive at least one should not have this()
+        this(3,"WOrld"); 
         System.out.println("int and int");
         this.i = a;
         this.j = b;
        
+    }
+    Constructor_Overload(int a,String s){
+        //this() //will be a loop error/recursive at least one should not have this()
+        System.out.println("int string");
+        this.i = a;
+        this.s = s;
     }
     Constructor_Overload(int a,int b,String s){
         this();
