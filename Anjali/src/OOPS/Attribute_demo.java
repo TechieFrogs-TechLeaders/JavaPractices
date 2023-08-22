780aa9ee88c8fd5ef4cd2b970831b2891d88525c
AnjaliCherukuri <mohananjalicherukuri@gmail.com>
2023-08-22 17:36:28 +0530

    new changes

--- Anjali/src/OOPS/Attribute_demo.java
+++ Anjali/src/OOPS/Attribute_demo.java
@@ -1,11 +1,19 @@
 package OOPS;
 
+import Method_Assign.Area_Square_Rectangle;
+
 public class Attribute_demo {
-    int x=20;
+    int x;
     public static void main(String[] args) {
         
         Attribute_demo myobj=new Attribute_demo();
         myobj.x=30;
         System.out.println(myobj.x);
+
+
+        //from method assign
+        Area_Square_Rectangle myarea = new Area_Square_Rectangle();
+        System.out.println(Area_Square_Rectangle.i); //Static
+        System.out.println(myarea.s); //public
     }
 }
