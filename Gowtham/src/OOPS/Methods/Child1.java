7774c6a74a84e5d785a0d27018c0affa3ccedfa7
gowthamcherukuri69 <gowtham.chg@gmail.com>
2023-08-23 17:44:41 +0530

    new changes

--- Gowtham/src/OOPS/Methods/Child1.java
+++ Gowtham/src/OOPS/Methods/Child1.java
@@ -1,11 +1,16 @@
 package OOPS.Methods;
 
-class B extends A {
+ class A {
+    public static void print() {
+        System.out.println("Parent");
+    }
+}
+
+ class B extends A {
     public static void print() {
         System.out.println("Child");
     }
 }
-
 class Main {
     public static void main(String[] args) {
         A obj = new B();
